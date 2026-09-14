be866eae32ff1a0763d3658afe594ecf64ec3872
Higor Moroni <higor.ferreira@melhorenvio.com>
2026-09-14 17:08:34 -0300

    feat: create auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TaskModule,
     UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { compareSync } from 'bcrypt';
+
+import { AuthResponseDto } from './auth.dto';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+  private jwtExpirationTimeInSeconds: number;
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {
+    this.jwtExpirationTimeInSeconds = +this.configService.getOrThrow<number>(
+      'JWT_EXPIRATION_TIME',
+    );
+  }
+
+  singIn(username: string, password: string): AuthResponseDto {
+    const foundUser = this.userService.findByUserName(username);
+
+    if (!foundUser || !compareSync(password, foundUser.password)) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = {
+      sub: foundUser.id,
+      username: foundUser.username,
+    };
+
+    const token = this.jwtService.sign(payload);
+
+    return {
+      token,
+      expiresIn: this.jwtExpirationTimeInSeconds,
+    };
+  }
+}
