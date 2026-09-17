caa79b5014f8c2f1f483ad1961a7ae44e8bf985d
Higor Moroni <higor.ferreira@melhorenvio.com>
2026-09-17 16:51:44 -0300

    feat: add postgres db and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     TaskModule,
     UserModule,
     AuthModule,
+    DbModule,
   ],
   controllers: [AppController],
   providers: [AppService],
